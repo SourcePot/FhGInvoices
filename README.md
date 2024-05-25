8e52921faf98830dec103bd2744ccc874afa09f5
Carsten Wallenhauer <115737488+SourcePot@users.noreply.github.com>
2024-05-25 13:04:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FhG invoices (Processor)
+This is a Datapool processor. The processor provides a user interface and data processing for entries containing parsed invoices.
+ 
---
+++ src/FhGInvoices.php
@@ -0,0 +1,223 @@
+<?php
+/*
+* This file is part of the Datapool CMS package.
+* @package Datapool
+* @author Carsten Wallenhauer <admin@datapool.info>
+* @copyright 2023 to today Carsten Wallenhauer
+* @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-v3
+*/
+declare(strict_types=1);
+
+namespace SourcePot;
+
+class FhGInvoices implements \SourcePot\Datapool\Interfaces\Processor{
+    
+    private $oc;
+
+    private $entryTable='';
+    private $entryTemplate=array('Read'=>array('type'=>'SMALLINT UNSIGNED','value'=>'ALL_MEMBER_R','Description'=>'This is the entry specific Read access setting. It is a bit-array.'),
+                                 'Write'=>array('type'=>'SMALLINT UNSIGNED','value'=>'ALL_CONTENTADMIN_R','Description'=>'This is the entry specific Read access setting. It is a bit-array.'),
+                                 );
+
+    public function __construct($oc){
+        $this->oc=$oc;
+        $table=str_replace(__NAMESPACE__,'',__CLASS__);
+        $this->entryTable=mb_strtolower(trim($table,'\\'));
+    }
+    
+    public function init(array $oc){
+        $this->oc=$oc;
+        $this->entryTemplate=$oc['SourcePot\Datapool\Foundation\Database']->getEntryTemplateCreateTable($this->entryTable,__CLASS__);
+    }
+    
+    public function getEntryTable():string{return $this->entryTable;}
+
+    /**
+     * This method is the interface of this data processing class
+     *
+     * @param array $callingElementSelector Is the selector for the canvas element which called the method 
+     * @param string $action Selects the requested process to be run  
+     *
+     * @return bool TRUE the requested action exists or FALSE if not
+     */
+    public function dataProcessor(array $callingElementSelector=array(),string $action='info'){
+        $callingElement=$this->oc['SourcePot\Datapool\Foundation\Database']->entryById($callingElementSelector,TRUE);
+        switch($action){
+            case 'run':
+                if (empty($callingElement)){
+                    return TRUE;
+                } else {
+                return $this->processInvoices($callingElement,$testRunOnly=FALSE);
+                }
+                break;
+            case 'test':
+                if (empty($callingElement)){
+                    return TRUE;
+                } else {
+                    return $this->processInvoices($callingElement,$testRunOnly=TRUE);
+                }
+                break;
+            case 'widget':
+                if (empty($callingElement)){
+                    return TRUE;
+                } else {
+                    return $this->getInvoicesWidget($callingElement);
+                }
+                break;
+            case 'settings':
+                if (empty($callingElement)){
+                    return TRUE;
+                } else {
+                    return $this->getInvoicesSettings($callingElement);
+                }
+                break;
+            case 'info':
+                if (empty($callingElement)){
+                    return TRUE;
+                } else {
+                    return $this->getInvoicesInfo($callingElement);
+                }
+                break;
+        }
+        return FALSE;
+    }
+
+    private function getInvoicesWidget($callingElement){
+        return $this->oc['SourcePot\Datapool\Foundation\Container']->container('Mapping','generic',$callingElement,array('method'=>'getInvoicesWidgetHtml','classWithNamespace'=>__CLASS__),array());
+    }
+
+    private function getInvoicesInfo($callingElement){
+        $matrix=array();
+        $matrix['']['value']='';
+        $html=$this->oc['SourcePot\Datapool\Tools\HTMLbuilder']->table(array('matrix'=>$matrix,'hideHeader'=>TRUE,'hideKeys'=>FALSE,'keep-element-content'=>TRUE,'caption'=>'Info'));
+        return $html;
+    }
+       
+    public function getInvoicesWidgetHtml($arr){
+        if (!isset($arr['html'])){$arr['html']='';}
+        // command processing
+        $result=array();
+        $formData=$this->oc['SourcePot\Datapool\Foundation\Element']->formProcessing(__CLASS__,__FUNCTION__);
+        if (isset($formData['cmd']['run'])){
+            $result=$this->processInvoices($arr['selector'],FALSE);
+        } else if (isset($formData['cmd']['test'])){
+            $result=$this->processInvoices($arr['selector'],TRUE);
+        }
+        // build html
+        $btnArr=array('tag'=>'input','type'=>'submit','callingClass'=>__CLASS__,'callingFunction'=>__FUNCTION__);
+        $matrix=array();
+        $btnArr['value']='Test';
+        $btnArr['key']=array('test');
+        $matrix['Commands']['Test']=$btnArr;
+        $btnArr['value']='Run';
+        $btnArr['key']=array('run');
+        $matrix['Commands']['Run']=$btnArr;
+        $arr['html'].=$this->oc['SourcePot\Datapool\Tools\HTMLbuilder']->table(array('matrix'=>$matrix,'style'=>'clear:left;','hideHeader'=>TRUE,'hideKeys'=>TRUE,'keep-element-content'=>TRUE,'caption'=>'Mapping'));
+        foreach($result as $caption=>$matrix){
+            $arr['html'].=$this->oc['SourcePot\Datapool\Tools\HTMLbuilder']->table(array('matrix'=>$matrix,'hideHeader'=>FALSE,'hideKeys'=>FALSE,'keep-element-content'=>TRUE,'caption'=>$caption));
+        }
+        $arr['wrapperSettings']=array('style'=>array('width'=>'fit-content'));
+        return $arr;
+    }
+
+
+    private function getInvoicesSettings($callingElement){
+        $html='';
+        if ($this->oc['SourcePot\Datapool\Foundation\Access']->isContentAdmin()){
+            $html.=$this->oc['SourcePot\Datapool\Foundation\Container']->container('Mapping entries settings','generic',$callingElement,array('method'=>'getInvoicesSettingsHtml','classWithNamespace'=>__CLASS__),array());
+        }
+        return $html;
+    }
+    
+    public function getInvoicesSettingsHtml($arr){
+        if (!isset($arr['html'])){$arr['html']='';}
+        $arr['html'].=$this->processingParams($arr['selector']);
+        $arr['html'].=$this->processingRules($arr['selector']);
+        //$selectorMatrix=$this->oc['SourcePot\Datapool\Tools\MiscTools']->arr2matrix($callingElement['Content']['Selector']);
+        //$arr['html'].=$this->oc['SourcePot\Datapool\Tools\HTMLbuilder']->table(array('matrix'=>$selectorMatrix,'style'=>'clear:left;','hideHeader'=>TRUE,'hideKeys'=>TRUE,'keep-element-content'=>TRUE,'caption'=>'Selector used for mapping'));
+        return $arr;
+    }
+
+    private function processingParams($callingElement){
+        $contentStructure=array('Target'=>array('method'=>'canvasElementSelect','excontainer'=>TRUE),
+                                'Order by'=>array('method'=>'keySelect','excontainer'=>TRUE,'value'=>'Date','standardColumsOnly'=>TRUE),
+                                'Order'=>array('method'=>'select','excontainer'=>TRUE,'value'=>0,'options'=>array(0=>'Descending',1=>'Ascending')),
+                                'Save'=>array('method'=>'element','tag'=>'button','element-content'=>'&check;','keep-element-content'=>TRUE,'value'=>'string'),
+                                );
+        $contentStructure['Order by']+=$callingElement['Content']['Selector'];
+        // get selctor
+        $arr=$this->oc['SourcePot\Datapool\Foundation\DataExplorer']->callingElement2arr(__CLASS__,__FUNCTION__,$callingElement,TRUE);
+        $arr['selector']=$this->oc['SourcePot\Datapool\Foundation\Database']->entryByIdCreateIfMissing($arr['selector'],TRUE);
+        // form processing
+        $formData=$this->oc['SourcePot\Datapool\Foundation\Element']->formProcessing(__CLASS__,__FUNCTION__);
+        $elementId=key($formData['val']);
+        if (isset($formData['cmd'][$elementId])){
+            $arr['selector']['Content']=$formData['val'][$elementId]['Content'];
+            $arr['selector']=$this->oc['SourcePot\Datapool\Foundation\Database']->updateEntry($arr['selector'],TRUE);
+        }
+        // get HTML
+        $arr['canvasCallingClass']=$callingElement['Folder'];
+        $arr['contentStructure']=$contentStructure;
+        $arr['caption']='Mapping control: Select mapping target and type';
+        $arr['noBtns']=TRUE;
+        $row=$this->oc['SourcePot\Datapool\Tools\HTMLbuilder']->entry2row($arr,FALSE,TRUE);
+        if (empty($arr['selector']['Content'])){$row['trStyle']=array('background-color'=>'#a00');}
+        $matrix=array('Parameter'=>$row);
+        return $this->oc['SourcePot\Datapool\Tools\HTMLbuilder']->table(array('matrix'=>$matrix,'style'=>'clear:left;','hideHeader'=>FALSE,'hideKeys'=>TRUE,'keep-element-content'=>TRUE,'caption'=>$arr['caption']));
+    }
+    
+    private function processingRules($callingElement){
+        $contentStructure=array('Target value or...'=>array('method'=>'element','tag'=>'input','type'=>'text','excontainer'=>TRUE),
+                                '...value selected by'=>array('method'=>'keySelect','excontainer'=>TRUE,'value'=>'useValue','addSourceValueColumn'=>TRUE,'addColumns'=>array('Linked file'=>'Linked file')),
+                                'Target data type'=>array('method'=>'select','excontainer'=>TRUE,'value'=>'string','options'=>$this->oc['SourcePot\Datapool\Tools\MiscTools']->getDataTypes(),'keep-element-content'=>TRUE),
+                                'Target column'=>array('method'=>'keySelect','excontainer'=>TRUE,'value'=>'Name','standardColumsOnly'=>TRUE),
+                                'Target key'=>array('method'=>'element','tag'=>'input','type'=>'text','excontainer'=>TRUE),
+                                'Compare value'=>array('method'=>'element','tag'=>'input','type'=>'text','excontainer'=>TRUE),
+                                );
+        $contentStructure['...value selected by']+=$callingElement['Content']['Selector'];
+        $contentStructure['Target column']+=$callingElement['Content']['Selector'];
+        $arr=$this->oc['SourcePot\Datapool\Foundation\DataExplorer']->callingElement2arr(__CLASS__,__FUNCTION__,$callingElement,TRUE);
+        $arr['canvasCallingClass']=$callingElement['Folder'];
+        $arr['contentStructure']=$contentStructure;
+        $arr['caption']='Mapping rules: Map selected entry values or constants (Source value) to target entry values';
+        $html=$this->oc['SourcePot\Datapool\Tools\HTMLbuilder']->entryListEditor($arr);
+        return $html;
+    }
+
+    private function processInvoices($callingElement,$testRun=FALSE){
+        $base=array('processingparams'=>array(),'processingrules'=>array());
+        $base=$this->oc['SourcePot\Datapool\Foundation\DataExplorer']->callingElement2settings(__CLASS__,__FUNCTION__,$callingElement,$base);
+        // loop through source entries and parse these entries
+        $this->oc['SourcePot\Datapool\Foundation\Database']->resetStatistic();
+        $result=array('Mapping statistics'=>array('Entries'=>array('value'=>0),
+                                                  'Spreadsheet entries'=>array('value'=>0),
+                                                  'Files added to zip'=>array('value'=>0),
+                                                  'Skip rows'=>array('value'=>0),
+                                                  'Output format'=>array('value'=>'Entries'),
+                                                 )
+                    );
+        // loop through entries
+        $params=current($base['processingparams']);
+        foreach($this->oc['SourcePot\Datapool\Foundation\Database']->entryIterator($callingElement['Content']['Selector'],TRUE,'Read') as $sourceEntry){
+            $result=$this->runEntry($base,$sourceEntry,$result,$testRun);
+        }
+        $result['Statistics']=$this->oc['SourcePot\Datapool\Foundation\Database']->statistic2matrix();
+        $result['Statistics']['Script time']=array('Value'=>date('Y-m-d H:i:s'));
+        $result['Statistics']['Time consumption [msec]']=array('Value'=>round((hrtime(TRUE)-$base['Script start timestamp'])/1000000));
+        return $result;
+    }
+    
+    private function runEntry($base,$sourceEntry,$result,$testRun){
+        $params=current($base['processingparams']);
+        $targetEntry=array();
+        $flatSourceEntry=$this->oc['SourcePot\Datapool\Tools\MiscTools']->arr2flat($sourceEntry);
+        foreach($base['processingrules'] as $ruleIndex=>$rule){
+        
+        }
+        return $result;
+    }
+    
+
+
+}
+?>
